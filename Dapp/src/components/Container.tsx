e12d340dbb32a0442d38e02030426d18abebaffa
Simon <hohlsim1@students.zhaw.ch>
2022-08-04 11:00:49 +0200

    add more trees

--- Dapp/src/components/Container.tsx
+++ Dapp/src/components/Container.tsx
@@ -1,17 +1,24 @@
-import { Canvas } from "@react-three/fiber";
+import { Camera, Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Field from "./Field";
 import "./Container.css"
 
+const camera = { 
+    fov: 100, 
+    near: 0.1, 
+    far: 1000, 
+    position: [0, 15, 20]
+}
+
 const Container = () => {
     return <div className="container">
         <div className="">spec</div>
-		<Canvas>
-            <OrbitControls /> 
-			<ambientLight />
-			<directionalLight color="red" intensity={10} />
+        <Canvas camera={ camera }>
+            <OrbitControls />
+            <ambientLight />
+            <directionalLight color="red" intensity={10} />
             <Field />
-		</Canvas>
+        </Canvas>
     </div>
 }
 
--- Dapp/src/components/Field.tsx
+++ Dapp/src/components/Field.tsx
@@ -31,6 +31,96 @@ const trees: IDetails[] = [
         harvestCounter: 0,
         wateringCounter: 0,
         lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
+    },
+    {
+        level: 0,
+        harvestCounter: 0,
+        wateringCounter: 0,
+        lastActionTimestamp: Date.now(),
     }
 ]
 
@@ -39,7 +129,12 @@ const Field = () => {
         {
             trees.map((tree: IDetails, index: number) => {
                 // return <Tree {...tree}/>
-                return <Tree key={index} position={[0,-2,0]} />
+                const zPos = Math.floor(index/5)*3;
+                const xPos = (index-5*Math.floor(index/5))*3;
+                const yPos = 0;
+                console.log(xPos, yPos, zPos);
+                const position = [xPos, yPos, zPos]
+                return <Tree key={index} position={position} />
             })
         }
     </>
