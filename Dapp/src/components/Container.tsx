07aaaaed20e4f14e9d3e0a72d1b49961ab1b91f9
Simon <hohlsim1@students.zhaw.ch>
2022-08-05 10:26:37 +0200

    remove new object creation

--- Dapp/src/components/Container.tsx
+++ Dapp/src/components/Container.tsx
@@ -2,7 +2,7 @@ import { Camera, Canvas, extend, ReactThreeFiber, useThree } from "@react-three/
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Field from "./Field";
 import "./Container.css"
-import { PerspectiveCamera, OrthographicCamera } from "three";
+import { PerspectiveCamera, OrthographicCamera, Euler } from "three";
 
 extend({ OrbitControls });
 
@@ -34,26 +34,21 @@ function Controls() {
 const Container = () => {
     const aspect = window.innerWidth / window.innerHeight;
     const d = 20;
-    const camera = new OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 1000 );
 
-    camera.position.set( 20, 20, 20 );
-    camera.rotation.order = 'YXZ';
-    camera.rotation.y = - Math.PI / 4;
-    camera.rotation.x = Math.atan( - 1 / Math.sqrt( 2 ) );
-    camera.zoom = 25;
-    
     return <div className="container">
         {/* <div className="">spec</div> */}
         <Canvas 
-            // camera={{ 
-            //     position: [0,10,30],
-            //     left: -2,
-            //     right: 2,
-            //     top: 2,
-            //     bottom: -2,
-            //     zoom: 25
-            // }} 
-            camera={camera}
+            camera={{ 
+                position: [20,20,20],
+                left:  - d * aspect,
+                right: d * aspect,
+                top: d,
+                bottom:  - d,
+                near: 1,
+                far: 10000,
+                zoom: 25,
+                rotation:  new Euler(Math.atan( - 1 / Math.sqrt( 2 ) ), - Math.PI / 4, 0, 'YXZ')
+            }} 
             orthographic
         >
             <ambientLight />
