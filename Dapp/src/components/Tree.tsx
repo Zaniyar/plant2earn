62916d73b815ca21766b59daf01bc674f42b40ea
Simon <hohlsim1@students.zhaw.ch>
2022-08-16 00:18:22 +0200

    growing tree

--- Dapp/src/components/Tree.tsx
+++ Dapp/src/components/Tree.tsx
@@ -14,9 +14,9 @@ import { IDetails } from "../interfaces/Interfaces.js";
 let configRaw = {
 	seed: 262,
 	segments: 6,
-	levels: 5,
+	levels: 0,
 	vMultiplier: 2.36,
-	twigScale: 0.39,
+	twigScale: 0,
 	initalBranchLength: 0.49,
 	lengthFalloffFactor: 0.85,
 	// lengthFalloffPower: 3,
@@ -27,14 +27,14 @@ let configRaw = {
 	dropAmount: -0.05,
 	growAmount: 0.235,
 	sweepAmount: 0.01,
-	maxRadius: 0.139,
+	maxRadius: 0,
 	climbRate: 0.371,
 	trunkKink: 0.093,
 	treeSteps: 5,
 	taperRate: 0.947,
 	radiusFalloffRate: 0.73,
 	twistRate: 3.02,
-	trunkLength: 2.4,
+	trunkLength: 0,
 	// custom
 	treeColor: 0x9d7362,
 	twigColor: 0xf16950
@@ -65,25 +65,62 @@ interface ITreeProps extends IDetails {
 };
 
 const Tree = (props: ITreeProps) => {
-
 	const { position, level, harvestCounter, wateringCounter } = props;
+
+	const planted = level >= 1;
+
+	const targetLevels = planted ? level + 1 : 0;
+	const targetTrunkLength =  planted ? 0.8 + level / 3  : 0;
+	const targetMaxRadius = planted ? 0.04 + level / 50 : 0;
+	const targetTwigScale =  planted ? wateringCounter / 10  : 0;
+
+	const [grow, setGrow] = useState(
+		{
+			levels: 0.01,
+			twigScale: 0.01,
+			maxRadius: 0.01,
+			trunkLength: 0.01
+		}
+	)
+
+	let timer: any;
+
+	const updateGrow = () => {
+		timer = !timer && setInterval(() => {
+			setGrow(prevCount => ({
+				levels: prevCount.levels >= targetLevels ? prevCount.levels : prevCount.levels += 0.1, 
+				trunkLength: prevCount.trunkLength >= targetTrunkLength ? prevCount.trunkLength : prevCount.trunkLength += 0.01, 
+				maxRadius: prevCount.maxRadius >= targetMaxRadius ? prevCount.maxRadius : prevCount.maxRadius += 0.01, 
+				twigScale: prevCount.twigScale >= targetTwigScale ? prevCount.twigScale : prevCount.twigScale += 0.01, 
+			}));
+		}, 100)
+
+		if (
+			grow.levels >= targetLevels &&
+			grow.trunkLength >=  targetTrunkLength &&
+			grow.maxRadius >=  targetMaxRadius &&
+			grow.twigScale >=  targetTwigScale
+		) {
+			clearInterval(timer)
+		}
+	}
+
+	config.levels = grow.levels;
+	config.trunkLength = grow.trunkLength;
+	config.maxRadius = grow.maxRadius;
+	config.twigScale = grow.twigScale;
+
+	useEffect(() => {
+		updateGrow()
+		return () => clearInterval(timer)
+	}, [props])
 
 	const ref = useRef();
 
 	useFrame(() => {
-		// let scale = 2;
-		// ref.current.scale.set(scale, scale, scale);
 		ref.current.rotation.y += 0.001;
-		// config.lengthFalloffPower -= 0.001;
+		// console.log(ref);
 	});
-
-	const planted = level >= 1;
-	if(planted){
-		config.levels = level + 1;
-		config.trunkLength = 0.8 + level / 3;
-		config.maxRadius = 0.04 + level / 50;
-		config.twigScale = wateringCounter / 10;
-	}
 
 	const tree = new Treex(config);
 	const treeGeometry = new THREE.BufferGeometry();
@@ -109,17 +146,18 @@ const Tree = (props: ITreeProps) => {
 	treeGroup.add(new THREE.Mesh(twigGeometry, twigMaterial));
 	treeGroup.scale.set(0.2, 0.2, 0.2);
 	oldTreeGroup = treeGroup;
-    const textureFarmland = useTexture(farmland) 
 
+	const textureFarmland = useTexture(farmland)
+	console.log(ref);
 	return (
 		<group ref={ref} position={position} args={[config.seed]}>
-			{ planted && <mesh geometry={treeGeometry} material={treeMaterial} /> }
-			{ planted && <mesh geometry={twigGeometry} material={twigMaterial} /> }
-            <mesh> 
-                <boxBufferGeometry args={[2,0,2]} />
-                <meshBasicMaterial map={textureFarmland}/>
-            </mesh>
-            {/* <Text color="black" anchorX="center" anchorY="middle">
+			{planted && <mesh geometry={treeGeometry} material={treeMaterial} />}
+			{planted && <mesh geometry={twigGeometry} material={twigMaterial} />}
+			<mesh>
+				<boxBufferGeometry args={[2, 0, 2]} />
+				<meshBasicMaterial map={textureFarmland} />
+			</mesh>
+			{/* <Text color="black" anchorX="center" anchorY="middle">
                 hello world!
             </Text> */}
 		</group>
